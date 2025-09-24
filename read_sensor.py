7d0b9a94b3bfdfdb945442b64ce15289930066f6
FlorianProdinger <FlorianProdinger@gmx.net>
2025-09-24 21:50:07 +0900

    update

--- read_sensor.py
+++ read_sensor.py
@@ -1,8 +1,14 @@
 import spidev
 import time
+import pandas as pd
+import numpy as np
+
 spi = spidev.SpiDev()
 spi.open( 0, 0 )
 spi.max_speed_hz = 1350000
+
+time_sleep = 0.05
+data_points = 10
 
 def read_channel(data):
     adc = spi.xfer( [ 1, (8+data) << 4 , 0] )
@@ -10,20 +16,43 @@ def read_channel(data):
     return data_
 
 
-def read_data( ):
+def read_data_full( mode="full"):
     channel_data_dic = {
             0 : [] ,
             1 : [] ,
             2 : [] ,
             }
     
-    for x in range(50):
-        for i  in range(3):
-            channel_data_dic[i].append( read_channel(i) )
-        time.sleep(0.05)
+    for x in range(data_points):
+            for i  in range(3):
+                channel_data_dic[i].append( read_channel(i) )
+            time.sleep(time_sleep)
+ 
+    
+    if mode=="full":
+        return channel_data_dic 
+    
+    elif mode=="normal":
+        return pd.DataFrame(channel_data_dic).mean()
+    else:
+        print("please enter 'full', 'normal' or 'test")
 
-    return channel_data_dic 
 
+def read_data():
+    channel_data_dic = {
+        'sensor_0': [],
+        'sensor_1': [],
+        'sensor_2': [],
+    }
+    
+    for x in range(data_points):
+        channel_data_dic['sensor_0'].append(read_channel(0))
+        channel_data_dic['sensor_1'].append(read_channel(1))
+        channel_data_dic['sensor_2'].append(read_channel(2))
+        time.sleep(time_sleep)
+
+    df = pd.DataFrame(channel_data_dic)
+    return df.mean()
 
 if __name__ == "__main__":
     data_dic_temp = read_data()
